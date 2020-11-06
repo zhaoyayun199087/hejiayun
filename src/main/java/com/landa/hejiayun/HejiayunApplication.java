74e216dab60bb986fd8ed4c0be61ee448c4e39af
limin <li.min@intellif.com>
2020-11-06 18:30:29 +0800

    新增登录接口

--- src/main/java/com/landa/hejiayun/HejiayunApplication.java
+++ src/main/java/com/landa/hejiayun/HejiayunApplication.java
@@ -1,13 +1,17 @@
 package com.landa.hejiayun;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.mashibing.controller"})
 public class HejiayunApplication {
 
     public static void main(String[] args) {
+        System.out.println("start application");
         SpringApplication.run(HejiayunApplication.class, args);
     }
 
 }
+
+
